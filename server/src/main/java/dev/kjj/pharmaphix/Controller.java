0c86b2ba71a3b354ae4d723429065caeb7a7e3ab
kokakarova <kostadinka.karova@appliedtechnology.se>
2024-03-25 14:20:58 +0100

    remove unused imports

--- server/src/main/java/dev/kjj/pharmaphix/Controller.java
+++ server/src/main/java/dev/kjj/pharmaphix/Controller.java
@@ -1,6 +1,5 @@
 package dev.kjj.pharmaphix;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
