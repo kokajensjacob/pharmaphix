d138eb972b4491ecf47681cc85238cb1ff156fad
jh-zzzzz <jacobheinegard@hotmail.com>
2024-03-26 10:31:39 +0100

    create loader function for GETting problem data

--- client/src/api.ts
+++ client/src/api.ts
@@ -6,3 +6,11 @@ export const getInvStatus = () => {
     .then((data) => data.needToBeOrdered)
     .catch(() => console.error("failed to fetch"));
 };
+
+export const getProblemData = (machine_id: string, problem_id: string) => {
+  return fetch(
+    `https://pharmaphix-server.azurewebsites.net/api/machines/${machine_id}/problems/${problem_id}`,
+  )
+    .then((resp) => resp.json())
+    .catch(() => console.error("failed to fetch problem data"));
+};
