202c12b0dd62a9c8577d9d2666f251022cb4ec2b
kokakarova <kostadinka.karova@appliedtechnology.se>
2024-03-26 09:55:40 +0100

    add ProblemPage component and new route

---
+++ client/src/components/ProblemPage.tsx
@@ -0,0 +1,7 @@
+export const ProblemPage = () => {
+    return (
+        <>
+        <h1>Issue Name</h1>  {/* will be replaced with fetch result */}
+        </>
+    )
+}
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -6,11 +6,13 @@ import {
 import { getInvStatus } from "./api";
 import { Home } from "./components/Home";
 import { RootLayout } from "./layouts/RootLayout";
+import { ProblemPage } from "./components/ProblemPage";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index loader={getInvStatus} element={<Home />}></Route>
+      <Route path="/machines/:machine_type_id/:problem_id" element={<ProblemPage />}></Route>
     </Route>,
   ),
 );
