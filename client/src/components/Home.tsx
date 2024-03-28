056f7cd3ad7493f80c14eab6188faeb173167b50
Jens Ortner <jens.ortner@appliedtechnology.se>
2024-03-28 13:08:49 +0100

    removed comment from home component

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { getInvStatus } from "../api";
 
 export const Home = () => {
-  // const orderQuantity = useLoaderData() as number;
   const [orderQuantity, setOrderQuantity] = useState();
 
   useEffect(() => {
