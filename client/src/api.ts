ac562718a12edd8d745da679e73a96d51dffe30a
jh-zzzzz <jacobheinegard@hotmail.com>
2024-03-28 17:06:09 +0100

    render machines from fetched data

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,6 +2,18 @@ import { SparePartDeductReqDto } from "./components/ProblemPage";
 
 const BASE_URL = "https://pharmaphix-server.azurewebsites.net/api";
 
+export const fetchMachines = async () => {
+  // return fetch(`${BASE_URL}/machines`)
+  //   .then((resp) => resp.json())
+  //   .catch(() => console.error("failed to fetch machines"));
+
+  return [
+    { machineId: "1", machineName: "Pill Press" },
+    { machineId: "2", machineName: "Another Machine" },
+    { machineId: "3", machineName: "Yet Another Machine" },
+  ];
+};
+
 export const getInvStatus = () => {
   return fetch(`${BASE_URL}/spare-parts/in-repair`)
     .then((resp) => resp.json())
--- client/src/components/MachinesPage.tsx
+++ client/src/components/MachinesPage.tsx
@@ -1,3 +1,36 @@
+import { useEffect, useState } from "react";
+import { fetchMachines } from "../api";
+import { Link } from "react-router-dom";
+
+type Machine = {
+  machineId: string;
+  machineName: string;
+};
+
 export const MachinesPage = () => {
-  return <></>;
+  const [machines, setMachines] = useState<Machine[]>();
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchMachines().then((data: Machine[]) => setMachines(data));
+    }, 3000);
+  });
+
+  return (
+    <>
+      <h1>Machines</h1>
+      {machines ? (
+        machines.map((machine) => (
+          <>
+            <Link to={machine.machineId} key={machine.machineId}>
+              {machine.machineName}
+            </Link>
+            <br />
+          </>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  );
 };
