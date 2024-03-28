6484e9d9d02a170f5ee0203835b8ae803ad42163
jh-zzzzz <jacobheinegard@hotmail.com>
2024-03-28 16:38:12 +0100

    create route for machines, create corresponding component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Nav } from "./components/Nav";
 import { Home } from "./components/Home";
 import { ProblemPage } from "./components/ProblemPage";
+import { MachinesPage } from "./components/MachinesPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/machines" element={<MachinesPage />} />
         <Route
           path="machines/:machine_type_id/:problem_id"
           element={<ProblemPage />}
