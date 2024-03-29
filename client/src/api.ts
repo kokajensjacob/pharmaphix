a14c2925f40e931fc762dbaebe49f719dbe55894
jh-zzzzz <jacobheinegard@hotmail.com>
2024-03-29 09:53:32 +0100

    make real fetch

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,46 +1,17 @@
-import { SparePartDeductReqDto } from "./pages/ProblemPage";
+import { SparePartDeductReqDto } from "./types";
 
 const BASE_URL = "https://pharmaphix-server.azurewebsites.net/api";
 
 export const fetchMachines = async () => {
-  // return fetch(`${BASE_URL}/machines`)
-  //   .then((resp) => resp.json())
-  //   .catch(() => console.error("failed to fetch machines"));
-
-  return [
-    { machineId: "1", machineName: "Pill Press" },
-    { machineId: "2", machineName: "Another Machine" },
-    { machineId: "3", machineName: "Yet Another Machine" },
-  ];
+  return fetch(`${BASE_URL}/machines`)
+    .then((resp) => resp.json())
+    .catch(() => console.error("failed to fetch machines"));
 };
 
 export async function fetchSpareParts() {
-  // return fetch(`${BASE_URL}/spare-parts`)
-  //   .then((resp) => resp.json())
-  //   .catch(() => console.error("failed to fetch spare parts"));
-
-  return [
-    {
-      id: "1",
-      name: "Laser X10",
-      quantityInStock: 5,
-      optimalQuantity: 5,
-      quantityInRepair: 1,
-      cost: 10000,
-      failureRate: 4,
-      repairTime: 0.5,
-    },
-    {
-      id: "2",
-      name: "Robotic Arm JK123PF3",
-      quantityInStock: 1,
-      optimalQuantity: 1,
-      quantityInRepair: 0,
-      cost: 1000000,
-      failureRate: 0.25,
-      repairTime: 0.5,
-    },
-  ];
+  return fetch(`${BASE_URL}/spare-parts`)
+    .then((resp) => resp.json())
+    .catch(() => console.error("failed to fetch spare parts"));
 }
 
 export const getInvStatus = () => {
