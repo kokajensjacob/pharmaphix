83a5e06880844fdca91030b57d0d7c282278f56e
jh-zzzzz <jacobheinegard@hotmail.com>
2024-04-01 19:43:56 +0200

    create page/route for adding new spare part

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { ProblemPage } from "./pages/ProblemPage";
 import { MachinesPage } from "./pages/MachinesPage";
 import { ProblemsPerMachinePage } from "./pages/ProblemsPerMachinePage";
 import { SparePartsPage } from "./pages/SparePartsPage";
+import { AddSparePartPage } from "./pages/AddSparePartPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/machines" element={<MachinesPage />} />
         <Route path="/spare-parts" element={<SparePartsPage />} />
+        <Route path="/spare-parts/add-new" element={<AddSparePartPage />} />
         <Route
           path="/machines/:machine_type_id"
           element={<ProblemsPerMachinePage />}
