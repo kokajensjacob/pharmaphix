95ee44db8e1e97ce6c5652657d385428d8282705
jh-zzzzz <jacobheinegard@hotmail.com>
2024-04-01 22:33:14 +0200

    create tsx skeleton for input form

--- client/src/pages/AddSparePartPage.tsx
+++ client/src/pages/AddSparePartPage.tsx
@@ -1,17 +1,23 @@
+import "../AddSparePartPage.css";
+import { AddSparePartForm } from "../components/AddSparePartForm";
+
 export const AddSparePartPage = () => {
   return (
-    <div className="text-sm breadcrumbs">
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/spare-parts">Spare Parts</a>
-        </li>
-        <li>
-          <a href="/spare-parts/add-new">Add new spare part</a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="text-sm breadcrumbs">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/spare-parts">Spare Parts</a>
+          </li>
+          <li>
+            <a href="/spare-parts/add-new">Add new spare part</a>
+          </li>
+        </ul>
+      </div>
+      <AddSparePartForm />
+    </>
   );
 };
