238426e8a3d11a8dd046d5c76f4854f850b2a6cc
jh-zzzzz <jacobheinegard@hotmail.com>
2024-04-01 21:27:33 +0200

    add breadcrumbs

--- client/src/pages/AddSparePartPage.tsx
+++ client/src/pages/AddSparePartPage.tsx
@@ -1,3 +1,17 @@
 export const AddSparePartPage = () => {
-  return <></>;
+  return (
+    <div className="text-sm breadcrumbs">
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/spare-parts">Spare Parts</a>
+        </li>
+        <li>
+          <a href="/spare-parts/add-new">Add new spare part</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
