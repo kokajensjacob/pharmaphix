14bb69f85493e808b8f8b2f19e68bbe207a84dc6
jh-zzzzz <jacobheinegard@hotmail.com>
2024-04-02 12:44:31 +0200

    grammar fix

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -26,22 +26,19 @@ export const Home = () => {
         <FetchError />
       ) : repairQuantity === undefined ? (
         <p>Loading...</p>
+      ) : repairQuantity === 0 ? (
+        <h1>
+          There are currently no spare parts undergoing repair in the workshop
+        </h1>
+      ) : repairQuantity === 1 ? (
+        <h1>
+          There is currently 1 spare part undergoing repair in the workshop
+        </h1>
       ) : (
-        repairQuantity === 0 ? (
-          <h1>
-            There are currently no spare parts undergoing repair in the workshop
-          </h1>
-        ) : repairQuantity === 1 ? (
-          <h1>
-            There are currently {repairQuantity} spare part undergoing repair in
-            the workshop
-          </h1>
-        ) : (
-          <h1>
-            There are currently {repairQuantity} spare parts undergoing repair
-            in the workshop
-          </h1>
-        )
+        <h1>
+          There are currently {repairQuantity} spare parts undergoing repair in
+          the workshop
+        </h1>
       )}
       <div className="home__button-container">
         <Link to="machines" className="btn btn-wide m-5">
