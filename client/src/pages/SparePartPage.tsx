f2b9f8ecfc5df565f53fb3172235e3d2e71cb0ea
jh-zzzzz <jacobheinegard@hotmail.com>
2024-04-02 17:02:25 +0200

    add breadcrumbs

--- client/src/pages/SparePartPage.tsx
+++ client/src/pages/SparePartPage.tsx
@@ -1,3 +1,28 @@
+import { useState } from "react";
+import { SparePart } from "../types";
+import { useParams } from "react-router-dom";
+
 export const SparePartPage = () => {
-  return <></>;
+  const [sparePart, setSparePart] = useState<SparePart>();
+  const { spare_part_id } = useParams();
+
+  return (
+    <>
+      <div className="text-sm breadcrumbs">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/spare-parts">Spare Parts</a>
+          </li>
+          <li>
+            <a href={`/spare-parts/${spare_part_id}`}>
+              {sparePart ? sparePart.name : "<spare_part_name>"}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
