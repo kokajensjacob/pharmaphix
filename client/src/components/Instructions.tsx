956080c0e9515c4e454f38189dc03321017e5305
Jens Ortner <jens.ortner@appliedtechnology.se>
2024-04-02 18:53:50 +0200

    add key to instructions divs

--- client/src/components/Instructions.tsx
+++ client/src/components/Instructions.tsx
@@ -4,9 +4,9 @@ type InstPRops = {
 
 export const Instructions = (props: InstPRops) => {
   const newText = props.instructions.split("§").map((str) => (
-    <div className="my-5 mx-5">
+    <div key={str} className="my-5 mx-5">
       {str.split("#").map((s) => (
-        <div>{s}</div>
+        <div key={s}>{s}</div>
       ))}
     </div>
   ));
