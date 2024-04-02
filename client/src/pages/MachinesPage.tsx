e4575944d55a8533caceef012e24793bd94079ad
kokakarova <kostadinka.karova@appliedtechnology.se>
2024-04-02 18:36:04 +0200

    add missing closing div

--- client/src/pages/MachinesPage.tsx
+++ client/src/pages/MachinesPage.tsx
@@ -32,20 +32,23 @@ export const MachinesPage = () => {
       ) : machines ? (
         <>
           <h1 className="text-xl font-medium m-5">Machines</h1>
-          {machines.sort((m1, m2) => {
+          {machines
+            .sort((m1, m2) => {
               let m1compare = m1.machineName.toUpperCase();
               let m2compare = m2.machineName.toUpperCase();
               return m1compare < m2compare ? -1 : m2compare < m1compare ? 1 : 0;
-            }).map((machine) => (
-            <div key={machine.machineId} className="inline-flex">
-              <div className="card w-72 h-72 bg-base-100 m-5 shadow">
-                <div className="card-body">
-                  <h2 className="card-title">{machine.machineName}</h2>
-                  <p>Quantity: {machine.machineQuantity}</p>
-                  <div className="card-actions justify-center">
-                    <Link to={machine.machineId}>
-                      <button className="btn">Related Problems</button>
-                    </Link>
+            })
+            .map((machine) => (
+              <div key={machine.machineId} className="inline-flex">
+                <div className="card w-72 h-72 bg-base-100 m-5 shadow">
+                  <div className="card-body">
+                    <h2 className="card-title">{machine.machineName}</h2>
+                    <p>Quantity: {machine.machineQuantity}</p>
+                    <div className="card-actions justify-center">
+                      <Link to={machine.machineId}>
+                        <button className="btn">Related Problems</button>
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
