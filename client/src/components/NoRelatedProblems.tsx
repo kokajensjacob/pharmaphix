f028db9563a735c24193f91735760907f02f4651
kokakarova <kostadinka.karova@appliedtechnology.se>
2024-04-02 18:42:52 +0200

    remove unneeded class

--- client/src/components/NoRelatedProblems.tsx
+++ client/src/components/NoRelatedProblems.tsx
@@ -1,7 +1,7 @@
 export const NoRelatedProblems = () => {
   return (
     <div className="bg-secondary-200 text-xl font-medium min-h-20 border text-center place-content-center my-10">
-      <div className="al">
+      <div>
         There are currently no related problems listed for this machine
       </div>
     </div>
