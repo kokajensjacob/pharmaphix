c88497c08bf64743d7d3f98fc196afd2f3eb3fa2
Jens Ortner <jens.ortner@appliedtechnology.se>
2024-04-02 12:45:46 +0200

    add column with badges to spare parts table

--- client/src/components/SparePartTable.tsx
+++ client/src/components/SparePartTable.tsx
@@ -13,6 +13,7 @@ export const SparePartTable = ({ spareParts }: { spareParts: SparePart[] }) => {
             <th>Purchase Cost</th>
             <th>Failure rate (per year)</th>
             <th>Repair time (years)</th>
+            <th>Inventory Status</th>
           </tr>
         </thead>
         <tbody>
@@ -25,6 +26,62 @@ export const SparePartTable = ({ spareParts }: { spareParts: SparePart[] }) => {
               <td>{sp.cost}</td>
               <td>{sp.failureRate}</td>
               <td>{sp.repairTime}</td>
+              <td>
+                {sp.quantityInRepair + sp.quantityInStock ===
+                sp.optimalQuantity ? (
+                  <div className="badge badge-success gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="inline-block w-4 h-4 stroke-current"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      ></path>
+                    </svg>
+                    Stock OK
+                  </div>
+                ) : sp.quantityInRepair + sp.quantityInStock >
+                  sp.optimalQuantity ? (
+                  <div className="badge badge-warning gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="inline-block w-4 h-4 stroke-current"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      ></path>
+                    </svg>
+                    Overstocked
+                  </div>
+                ) : (
+                  <div className="badge badge-error gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="inline-block w-4 h-4 stroke-current"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      ></path>
+                    </svg>
+                    Understocked
+                  </div>
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
