8c0a2d9bd8245c78ec4648eb85d749588a39b831
jh-zzzzz <jacobheinegard@hotmail.com>
2024-04-02 10:13:20 +0200

    implement user dialog based on post response

--- client/src/components/AddSparePartForm.tsx
+++ client/src/components/AddSparePartForm.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from "react";
 import { Machine, SparePartPostRequest } from "../types";
 import { postNewSparePart } from "../api";
+import { PatchUserDialog } from "./PatchUserDialog";
 
 type NewSparePartFormEvent = FormEvent<HTMLFormElement> & {
   target: {
@@ -16,6 +17,21 @@ type NewSparePartFormEvent = FormEvent<HTMLFormElement> & {
 
 export const AddSparePartForm = ({ machines }: { machines: Machine[] }) => {
   const [disableForm, setDisableForm] = useState<boolean>(false);
+  const [userDialog, setUserDialog] = useState<{
+    showMessage: boolean;
+    message: string;
+  }>({ showMessage: false, message: "" });
+
+  function resetUserDialog() {
+    setUserDialog({ showMessage: false, message: "" });
+  }
+
+  function displayUserDialog(message: string, duration: number) {
+    setUserDialog({ showMessage: true, message: message });
+    setTimeout(() => {
+      resetUserDialog();
+    }, duration);
+  }
 
   function handleOnSubmit(e: NewSparePartFormEvent) {
     e.preventDefault();
@@ -34,21 +50,36 @@ export const AddSparePartForm = ({ machines }: { machines: Machine[] }) => {
     postNewSparePart(request)
       .then((resp) => {
         switch (resp.status) {
-          case 201: // TO DO
-            console.log("successfully added");
+          case 201:
+            displayUserDialog("Successful", 5000);
             break;
-          case 400: // TO DO
-            console.error("bad request");
+          case 400:
+            displayUserDialog(
+              "Spare part was not created because the request was bad",
+              5000,
+            );
             break;
-          default: // TO DO
-            console.log("something unexpected happened");
+          case 404:
+            displayUserDialog(
+              "Couldn't find the associated machine in the database. It might have been recently deleted by another user. Try reloading the page",
+              10000,
+            );
+            break;
+          case 409:
+            displayUserDialog(
+              "That spare part already exists in the database!",
+              5000,
+            );
+            break;
+          default:
+            displayUserDialog("Something unexpected happened", 5000);
         }
       })
       .then(() => setDisableForm(false))
       .catch(() => {
+        displayUserDialog("Server unavailable. Try again later", 3000);
         setDisableForm(false);
-        console.error("couldnt post yo");
-      }); // TO DO
+      });
   }
 
   function getFailureRate(amount: number, timeUnit: "week" | "month" | "year") {
@@ -68,105 +99,110 @@ export const AddSparePartForm = ({ machines }: { machines: Machine[] }) => {
   }
 
   return (
-    <form onSubmit={handleOnSubmit}>
-      <label htmlFor="sparePartName" className="form-control w-full max-w-xs">
-        <div className="label">
-          <span className="label-text">Spare part name *</span>
+    <>
+      <form onSubmit={handleOnSubmit}>
+        <label htmlFor="sparePartName" className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Spare part name *</span>
+          </div>
+        </label>
+        <input
+          type="text"
+          name="sparePartName"
+          id="sparePartName"
+          disabled={disableForm}
+          className="input input-bordered w-full max-w-xs"
+        />
+        <label htmlFor="sparePartCost" className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Purchase cost per spare part *</span>
+          </div>
+        </label>
+        <input
+          type="text"
+          id="sparePartCost"
+          placeholder="1194.99"
+          disabled={disableForm}
+          className="input input-bordered w-full max-w-xs"
+        />
+        <label
+          htmlFor="sparePartFailureRate"
+          className="form-control w-full max-w-xs"
+        >
+          <div className="label">
+            <span className="label-text">
+              How often does the spare part need replacing? (on average) *
+            </span>
+          </div>
+        </label>
+        <div id="sparePartFailureRate">
+          <input
+            type="number"
+            name=""
+            id="failAmount"
+            defaultValue={1}
+            min={1}
+            disabled={disableForm}
+          />
+          /
+          <select defaultValue="month" id="failDivisor" disabled={disableForm}>
+            <option value="week">week</option>
+            <option value="month">month</option>
+            <option value="year">year</option>
+          </select>
         </div>
-      </label>
-      <input
-        type="text"
-        name="sparePartName"
-        id="sparePartName"
-        disabled={disableForm}
-        className="input input-bordered w-full max-w-xs"
-      />
-      <label htmlFor="sparePartCost" className="form-control w-full max-w-xs">
-        <div className="label">
-          <span className="label-text">Purchase cost per spare part *</span>
-        </div>
-      </label>
-      <input
-        type="text"
-        id="sparePartCost"
-        placeholder="1194.99"
-        disabled={disableForm}
-        className="input input-bordered w-full max-w-xs"
-      />
-      <label
-        htmlFor="sparePartFailureRate"
-        className="form-control w-full max-w-xs"
-      >
-        <div className="label">
-          <span className="label-text">
-            How often does the spare part need replacing? (on average) *
-          </span>
-        </div>
-      </label>
-      <div id="sparePartFailureRate">
+        <label
+          htmlFor="sparePartInStock"
+          className="form-control w-full max-w-xs"
+        >
+          <div className="label">
+            <span className="label-text">How many are in stock right now?</span>
+          </div>
+        </label>
         <input
           type="number"
-          name=""
-          id="failAmount"
-          defaultValue={1}
-          min={1}
+          name="sparePartInStock"
+          id="sparePartInStock"
           disabled={disableForm}
         />
-        /
-        <select defaultValue="month" id="failDivisor" disabled={disableForm}>
-          <option value="week">week</option>
-          <option value="month">month</option>
-          <option value="year">year</option>
-        </select>
-      </div>
-      <label
-        htmlFor="sparePartInStock"
-        className="form-control w-full max-w-xs"
-      >
-        <div className="label">
-          <span className="label-text">How many are in stock right now?</span>
-        </div>
-      </label>
-      <input
-        type="number"
-        name="sparePartInStock"
-        id="sparePartInStock"
-        disabled={disableForm}
-      />
-      <label
-        htmlFor="sparePartRepairTime"
-        className="form-control w-full max-w-xs"
-      >
-        <div className="label">
-          <span className="label-text">
-            Enter average repair time of spare part:
-          </span>
-        </div>
-      </label>
-      <input
-        type="number"
-        name="sparePartRepairTime"
-        id="sparePartRepairTime"
-        disabled={disableForm}
-      />{" "}
-      years
-      <fieldset disabled={disableForm}>
-        <legend className="label-text">
-          Select the associated machine from the list below
-        </legend>
-        {machines.map(({ machineId, machineName }) => (
-          <label className="label cursor-pointer" key={machineId}>
-            <span className="label-text">{machineName}</span>
-            <input
-              type="radio"
-              name="associatedMachineId"
-              value={machineId}
-              className="radio checked:bg-blue-500"
-            />
-          </label>
-        ))}
-      </fieldset>
-      <input type="submit" className="btn" value="Add new spare part" />
-    </form>
+        <label
+          htmlFor="sparePartRepairTime"
+          className="form-control w-full max-w-xs"
+        >
+          <div className="label">
+            <span className="label-text">
+              Enter average repair time of spare part:
+            </span>
+          </div>
+        </label>
+        <input
+          type="number"
+          name="sparePartRepairTime"
+          id="sparePartRepairTime"
+          disabled={disableForm}
+        />{" "}
+        years
+        <fieldset disabled={disableForm}>
+          <legend className="label-text">
+            Select the associated machine from the list below
+          </legend>
+          {machines.map(({ machineId, machineName }) => (
+            <label className="label cursor-pointer" key={machineId}>
+              <span className="label-text">{machineName}</span>
+              <input
+                type="radio"
+                name="associatedMachineId"
+                value={machineId}
+                className="radio checked:bg-blue-500"
+              />
+            </label>
+          ))}
+        </fieldset>
+        <input type="submit" className="btn" value="Add new spare part" />
+      </form>
+      {userDialog.showMessage && (
+        <PatchUserDialog message={userDialog.message} />
+      )}
+    </>
   );
 };
