b4c8cba6718188693a0022749c44d2f5d0567d92
Jens Ortner <jens.ortner@appliedtechnology.se>
2024-04-03 16:59:36 +0200

    Change response dto

--- server/src/main/java/dev/kjj/pharmaphix/dtos/MachineProblemsResponseDto.java
+++ server/src/main/java/dev/kjj/pharmaphix/dtos/MachineProblemsResponseDto.java
@@ -7,12 +7,14 @@ import java.util.List;
 public record MachineProblemsResponseDto(
         String machineName,
         int machineQuantity,
+        double machineCost,
         List<ProblemListDto> problems
 ) {
     public static MachineProblemsResponseDto convertToDto(Machine machine) {
         return new MachineProblemsResponseDto(
                 machine.getName(),
                 machine.getQuantity(),
+                machine.getCost(),
                 machine.getProblems().stream()
                         .map(ProblemListDto::convertToDto)
                         .toList()
