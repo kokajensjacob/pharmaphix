12010b0546386e3590ff433e87786c024944cfda
Jens Ortner <jens.ortner@appliedtechnology.se>
2024-04-03 14:11:06 +0200

    center buttons on navbar

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -2,20 +2,20 @@ import { Link, NavLink, useLocation } from "react-router-dom";
 
 export const Nav = () => {
   return (
-    <div className="navbar bg-base-300">
-      <div className="flex-1">
+    <div className="navbar bg-base-300 grid-cols-3">
+      <div className="flex w-1/3">
         <Link to="/" className="btn btn-ghost text-xl">
           PharmaPhix
         </Link>
       </div>
       {useLocation().pathname !== "/" ? (
-        <div className="flex-none">
+        <div className="flex w-1/3 justify-center">
           <ul className="menu menu-horizontal px-1">
             <li>
               <NavLink
                 to="/machines"
                 className={({ isActive, isPending }) =>
-                  isPending ? "pending" : isActive ? "active" : ""
+                  isPending ? "pending mr-3" : isActive ? "active mr-3" : ""
                 }
               >
                 <div>Machines</div>
@@ -25,7 +25,7 @@ export const Nav = () => {
               <NavLink
                 to="/spare-parts"
                 className={({ isActive, isPending }) =>
-                  isPending ? "pending" : isActive ? "active" : ""
+                  isPending ? "pending ml-3" : isActive ? "active ml-3" : ""
                 }
               >
                 <div>Spare Parts</div>
